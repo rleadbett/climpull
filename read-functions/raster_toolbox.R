9e453676c47f0c9ee3c2fdc9594b0a4b8f6b6ca2
Ryan Leadbetter <ryan.leadbetter@postgrad.curtin.edu.au>
2023-09-26 10:13:15 +0800

    Create functions to query the NOAA ERDDAP servers.

---
+++ read-functions/raster_toolbox.R
@@ -0,0 +1,125 @@
+library(ncdf4)
+library(rerddap)
+library(terra)
+library(dplyr)
+library(stringr)
+
+# write script to convert to raster
+ncdfToRast <- function(ncdf_list, var) {
+    # convert to spatRaster
+    rast_list <- lapply(
+       ncdf_list,
+       function(ncdf) ncdfFileToRast(ncdf, var) 
+    )
+
+    # make sure all rasters have same extent
+    if (length(rast_list) > 1) {
+        for (i in 2:length(rast_list)) {
+            if(ext(rast_list[[1]]) != ext(rast_list[[i]])) {
+                rast_list[[i]] <- resample(
+                    rast_list[[i]], 
+                    rast_list[[1]], 
+                    threads = TRUE
+                )
+            }
+        }
+    }
+    
+    # combine rasters into one stack
+    full_rast <- rast(rast_list)
+    
+    # sort by date
+    full_rast <- full_rast[[order(time(full_rast))]]
+    
+    # fill missing values
+    full_rast_clean <- focal(
+        full_rast, 
+        w=3, 
+        fun=mean, 
+        na.policy = "only", 
+        na.rm=TRUE
+    )
+    full_rast_clean <- approximate(full_rast_clean)
+
+
+    return(full_rast_clean)
+}
+
+ncdfFileToRast <- function(ncdf_obj, var) {
+    lon <- unique(ncdf_obj$data$longitude)
+    lat <- unique(ncdf_obj$data$latitude)
+    time <- unique(ncdf_obj$data$time)
+
+    ras <- terra::rast(
+        nrow = length(lat), 
+        ncol = length(lon), 
+        ymin = min(lat), 
+        ymax = max(lat), 
+        xmin = min(lon), 
+        xmax = max(lon),
+        nlyrs = length(time),
+        crs = "+proj=utm +zone=48 +datum=WGS84",
+        time = lubridate::ymd_hms(time)
+    )
+
+    values(ras) <- ncdf_obj$data %>%
+    arrange(time, desc(latitude), longitude) %>%
+    pull(matches(var))
+
+    ras <- fillTime(ras)
+
+    return(ras)
+}
+
+
+rastStackToMov <- function(stacked_raster, shape_file = NULL, output_file) {
+    # This function requires the command line tool ImageMagick: 
+    # https://imagemagick.org/script/command-line-tools.php
+    
+    # make temporary directory to save plots
+    dir.create("temp")
+
+    # crop plots
+    if (!is.null(shape_file)) {
+        v_crop <- crop(x = shape_file, y = ext(stacked_raster))
+        crs(v_crop) <- "+proj=utm +zone=48 +datum=WGS84"
+
+        stacked_raster <- mask(
+            stacked_raster,
+            v_crop,
+            inverse = TRUE,
+            touches = FALSE
+        )
+    }
+    
+    # Plot each frame and save to .png
+    times <- time(stacked_raster)
+    val_range <- range(values(stacked_raster), na.rm = TRUE)
+
+    for (i in 1:length(times)) {
+        png(
+            file = file.path(
+                "./temp", 
+                str_c(str_remove_all(times[i], "(:| )"), ".png")
+            ),
+            width = 600, 
+            height = 600
+        )
+        plot(
+            stacked_raster[[i]], 
+            main = as.character(times[i]), 
+            range = val_range
+        )
+        if (!is.null(shape_file)) plot(v_crop, add = TRUE)
+
+        dev.off()
+    }
+
+    # write system command
+    cmd = paste0('magick', ' ', './temp', '/*.png', ' ', output_file)
+
+    # execute system command
+    system(cmd)
+
+    unlink("temp", recursive = T)
+}
---
+++ read-functions/read_chl_a.R
@@ -0,0 +1,86 @@
+library(ncdf4)
+library(rerddap)
+library(terra)
+library(dplyr)
+library(stringr)
+
+source("./read-functions/read_ERDAP.R", chdir = TRUE)
+source("./read-functions/raster_toolbox.R", chdir = TRUE)
+
+
+servers() %>% View()
+
+ed_search_adv(
+    query = 'current',
+    minLat = -25,
+    maxLat = -15,
+    minLon = 115,
+    maxLon = 125,
+    protocol = "griddap",
+    url = "https://coastwatch.noaa.gov/erddap/"
+) %>% View()
+
+ed_search_adv(
+    query = 'current',
+    minLat = -25,
+    maxLat = -15,
+    minLon = 115,
+    maxLon = 125,
+    protocol = "griddap",
+    url = "https://www.ncei.noaa.gov/erddap/"
+) %>% View()
+
+ed_search_adv(
+    query = 'sst',
+    minLat = -25,
+    maxLat = -15,
+    minLon = 115,
+    maxLon = 125,
+    protocol = "griddap",
+    url = "https://www.ncei.noaa.gov/erddap/"
+) %>% View()
+
+ed_search_adv(
+    query = 'chl',
+    minLat = -25,
+    maxLat = -15,
+    minLon = 115,
+    maxLon = 125,
+    protocol = "griddap",
+    url = "https://coastwatch.pfeg.noaa.gov/erddap/"
+) %>% View()
+
+
+ed_search_adv(
+    query = 'sss',
+    minLat = -25,
+    maxLat = -15,
+    minLon = 115,
+    maxLon = 125,
+    protocol = "griddap",
+    url = "https://coastwatch.pfeg.noaa.gov/erddap/"
+) %>% View()
+
+
+test_chl_a <- getNCDF(
+    ds_ids = c(
+        # Aqua MODIS
+        "erdMH1chla1day",
+        # SeaWiFS
+        "erdSW2018chla1day"#,
+        # Nasa
+        #"erdVH2018chla1day"
+    ), 
+    time_range = c("2002-01-01", "2004-01-01"), 
+    lat_range = c(-25, -15), 
+    lon_range = c(115, 125)
+    
+)
+
+ras_clean <- ncdfToRast(ncdf_list = test_chl_a, var = "chl")
+
+rastStackToMov(
+    ras_clean,
+    shape_file = terra::vect("./data/Australia_shp/STE11aAust.shp"),
+    output_file = "./static/test.gif"
+)
