17f7e82cbf933a16fde0e21acad55c0feb543492
rleadbett <ryan.leadbetter@student.curtin.edu.au>
2023-10-04 10:44:13 +0000

    Read ncdf by chunks.

--- read-functions/read_ERDDAP.R
+++ read-functions/read_ERDDAP.R
@@ -3,127 +3,211 @@ library(rerddap)
 library(terra)
 library(dplyr)
 library(stringr)
+library(lubridate)
 
-getNCDF <- function(ds_ids, time_range, lat_range, lon_range, ...) {
-    lat_range <- lat_range[order(lat_range, decreasing = FALSE)]
-    lon_range <- lon_range[order(lon_range, decreasing = FALSE)]
+getNCDF <- function(
+  ds_ids,
+  time_range,
+  lat_range,
+  lon_range,
+  ...
+) {
+  # re-order lat/lon in the case that they are entered wrong
+  lat_range <- lat_range[order(lat_range, decreasing = FALSE)]
+  lon_range <- lon_range[order(lon_range, decreasing = FALSE)]
 
-    # get the info
-    out <- lapply(
-        ds_ids,
-        function(id) info(id, ...)
+  # get the info of the nc file
+  out <- lapply(
+    ds_ids,
+    function(id) info(id, ...)
+  )
+
+  # info list by date
+  if (length(out) > 1) {
+    out <- sortInfo(
+      list_of_ranges = lapply(
+        out,
+        function(ds) getTimeRange(ds)
+      ),
+      erddap_info_list = out
     )
+  }
 
-    # sort by date
-    if (length(out) > 1) {
-        out <- sortInfo(
-            list_of_ranges = lapply(
-                out,
-                function(ds) getTimeRange(ds)
-            ),
-            erddap_info_list = out
-        )
-    }
+  # prepare time query
+  nc_files <- callErddap(
+    erddap_info_list = out,
+    time_query = prepareQuery(time_range, out),
+    lat_range = lat_range,
+    lon_range = lon_range,
+    ...
+  )
 
-    # prepare time query
-
-    nc_files <- callErddap(
-        erddap_info_list = out,
-        time_query = prepareQuery(time_range, out),
-        lat_range = lat_range,
-        lon_range = lon_range,
-        ...
-    )
-
-    return(nc_files)
+  return(nc_files)
 }
 
 getTimeRange <- function(erddap_info_obj) {
-    # function to get the date range of data
-    global <- erddap_info_obj$alldata$NC_GLOBAL
-    tt <- global[global$attribute_name %in% c("time_coverage_end", "time_coverage_start"), "value", ]
-    return(c(tt[2], tt[1]))
+  # function to get the date range of data
+  global <- erddap_info_obj$alldata$NC_GLOBAL
+  global_att <- global$attribute_name %in%
+    c("time_coverage_end", "time_coverage_start")
+  tt <- global[global_att, "value", ]
+  start_date <- as.character(date(tt[2]))
+  end_date <- as.character(date(tt[1]))
+  return(c(start_date, end_date))
 }
 
 sortInfo <- function(list_of_ranges, erddap_info_list) {
-    time_order <- lapply(
-        list_of_ranges,
-        function(tt) tt[2]
-    ) %>%
-        unlist() %>%
-        order(decreasing = TRUE)
+  time_order <- lapply(
+    list_of_ranges,
+    function(tt) tt[2]
+  ) %>%
+    unlist() %>%
+    order(decreasing = TRUE)
 
-    erddap_info_sorted <- erddap_info_list[time_order]
+  erddap_info_sorted <- erddap_info_list[time_order]
 
-    return(erddap_info_sorted)
+  return(erddap_info_sorted)
 }
 
-prepareQuery <- function(time_range, erddap_info_list) {
-    time_range <- time_range[order(time_range, decreasing = FALSE)]
+prepareQuery <- function(time_range, erddap_info_list, chucks) {
+  time_range <- time_range[order(time_range, decreasing = FALSE)]
 
-    # get ranges
-    info_time_range <- lapply(erddap_info_list, getTimeRange)
+  # get ranges
+  info_time_range <- lapply(erddap_info_list, getTimeRange)
 
-    # adjust for overlapping data sets
-    if (length(info_time_range) > 1) {
-        for (i in 1:(length(info_time_range) - 1)) {
-            if (info_time_range[[i]][1] < info_time_range[[i + 1]][2]) {
-                info_time_range[[i + 1]][2] <- info_time_range[[i]][1]
-            }
-        }    
+  # adjust for overlapping data sets
+  if (length(info_time_range) > 1) {
+    for (i in 1:(length(info_time_range) - 1)) {
+      if (info_time_range[[i]][1] < info_time_range[[i + 1]][2]) {
+        info_time_range[[i + 1]][2] <- info_time_range[[i]][1]
+      }
     }
+  }
 
-    # check time_range overlap
-    time_range_check <- lapply(
-        time_range,
-        function(time) {
-            lapply(
-                info_time_range,
-                function(range) {
-                    between(
-                        time,
-                        range[1],
-                        range[2]
-                    )
-                }
-            ) %>% unlist()
+  # check time_range overlap
+  time_range_check <- lapply(
+    time_range,
+    function(time) {
+      lapply(
+        info_time_range,
+        function(range) {
+          between(
+            time,
+            range[1],
+            range[2]
+          )
         }
-    )
+      ) %>% unlist()
+    }
+  )
 
-    # change start date
-    data_set_range <- c(
-        which(time_range_check[[1]]),
-        which(time_range_check[[2]])
-    )
+  # change start date
+  data_set_range <- c(
+    which(time_range_check[[1]]),
+    which(time_range_check[[2]])
+  )
 
-    time_query <- as.list(rep(NA, length(info_time_range)))
-    time_query[data_set_range[1]:data_set_range[2]] <- info_time_range[data_set_range[1]:data_set_range[2]]
-    time_query[[data_set_range[1]]][1] <- time_range[1]
-    time_query[[data_set_range[2]]][2] <- time_range[2]
+  time_query <- as.list(rep(NA, length(info_time_range)))
+  time_query[data_set_range[1]:data_set_range[2]] <-
+    info_time_range[data_set_range[1]:data_set_range[2]]
+  time_query[[data_set_range[1]]][1] <- time_range[1]
+  time_query[[data_set_range[2]]][2] <- time_range[2]
 
-    return(time_query)
+  return(time_query)
 }
 
-callErddap <- function(erddap_info_list, time_query, lat_range, lon_range, ...) {
-    data_sets_in_time_window <- lapply(
-        time_query,
-        function(tt) sum(is.na(tt)) == 0
-    ) %>%
-        unlist() %>%
-        which()
+callErddap <- function(
+  erddap_info_list,
+  time_query,
+  lat_range,
+  lon_range,
+  ...
+) {
+  data_sets_in_time_window <- lapply(
+    time_query,
+    function(tt) sum(is.na(tt)) == 0
+  ) %>%
+    unlist() %>%
+    which()
 
-    nc_files <- lapply(
-        data_sets_in_time_window,
-        function(i) {
-            griddap(
-                erddap_info_list[[i]],
-                latitude = lat_range,
-                longitude = lon_range,
-                time = time_query[[i]],
-                ...
-            )
-        }
+  if ("chunks" %in% ...names()) {
+    chunks <- ...elt(which(...names() == "chunks"))
+
+    new_query <- chunkQuery(
+      chunks = chunks,
+      time_query = time_query,
+      erddap_info_list = erddap_info_list
     )
+    erddap_info_list <- new_query[[1]]
+    time_query <- new_query[[2]]
+    data_sets_in_time_window <- new_query[[3]]
+  }
 
-    return(nc_files)
+  nc_files <- lapply(
+    data_sets_in_time_window,
+    function(i) {
+      griddap(
+        erddap_info_list[[i]],
+        latitude = lat_range,
+        longitude = lon_range,
+        time = str_c(time_query[[i]], "T12:00:00Z")#,
+        #store = memory()
+      )
+    }
+  )
+
+  return(nc_files)
+}
+
+chunkQuery <- function(
+  chunks,
+  time_query,
+  erddap_info_list
+) {
+  # check inputs
+  if (!is.period(chunks)) {
+    stop("The input 'chunks' must be a lubridate period.")
+  }
+  # get valid time_queries
+  time_query_new <- list()
+  erddap_info_list_new <- list()
+  data_sets_in_time_window_new <- c()
+  i <- 1
+  for (j in 1:length(time_query)){
+    if (is.na(time_query[j])) {
+      time_query_new[[i]] <- NA
+      erddap_info_list_new[[i]] <- erddap_info_list[[j]]
+      i <- i + 1
+    } else {
+      time_chunk <- ymd(time_query[[j]][1])
+      t_end <- ymd(time_query[[j]][2])
+      while ((time_chunk + chunks) < t_end) {
+        time_query_new[[i]] <- c(
+          as.character(time_chunk),
+          as.character(time_chunk + chunks - days(1))
+        )
+        erddap_info_list_new[[i]] <- erddap_info_list[[j]]
+        data_sets_in_time_window_new <- append(
+          data_sets_in_time_window_new,
+          i
+        )
+        time_chunk <- time_chunk + chunks
+        i <- i + 1
+      }
+      time_query_new[[i]] <- c(
+        as.character(time_chunk),
+        as.character(t_end)
+      )
+      erddap_info_list_new[[i]] <- erddap_info_list[[j]]
+      data_sets_in_time_window_new <- append(
+        data_sets_in_time_window_new,
+        i
+      )
+      i <- i + 1
+    }
+  }
+
+  return(
+    list(erddap_info_list_new, time_query_new, data_sets_in_time_window_new)
+  )
 }
--- read-functions/read_chl_a.R
+++ read-functions/read_chl_a.R
@@ -4,13 +4,46 @@ library(terra)
 library(dplyr)
 library(stringr)
 
+setwd("./climpull/")
+
 source("./read-functions/read_ERDDAP.R", chdir = TRUE)
 source("./read-functions/raster_toolbox.R", chdir = TRUE)
+
+time_window <- c("2000-01-01", "2022-07-27")
+latitude_range <- c(-25, -5)
+longitude_range <- c(110, 135)
+
+chl_a_nc <- getNCDF(
+    ds_ids = c(
+        # Aqua MODIS
+        "erdMH1chla1day",
+        # SeaWiFS
+        "erdSW2018chla1day" # ,
+        # Nasa
+        # "erdVH2018chla1day"
+    ),
+    time_range = time_window,
+    lat_range = latitude_range,
+    lon_range = longitude_range,
+    chunks = years(2)
+)
+
+chl_a_ras <- ncdfToRast(chl_a_nc)
+
+sst_nc <- getNCDF(
+    ds_ids = "nceiPH53sstn1day",
+    url = "https://coastwatch.pfeg.noaa.gov/erddap/",
+    time_range = time_window,
+    lat_range = latitude_range,
+    lon_range = longitude_range
+)
+
+sst_ras <- ncdfToRast(sst_nc, var = "sea_surface_temperature")
 
 servers() %>% View()
 
 ed_search_adv(
-    query = 'current',
+    query = "current",
     minLat = -25,
     maxLat = -15,
     minLon = 115,
@@ -20,16 +53,7 @@ ed_search_adv(
 ) %>% View()
 
 ed_search_adv(
-    query = 'salinity',
-    minLat = -25,
-    maxLat = -15,
-    minLon = 115,
-    maxLon = 125,
-    url = "https://polarwatch.noaa.gov/erddap/"
-) %>% View()
-
-ed_search_adv(
-    query = 'sst',
+    query = "current",
     minLat = -25,
     maxLat = -15,
     minLon = 115,
@@ -39,7 +63,17 @@ ed_search_adv(
 ) %>% View()
 
 ed_search_adv(
-    query = 'chl',
+    query = "sst",
+    minLat = -25,
+    maxLat = -15,
+    minLon = 115,
+    maxLon = 125,
+    protocol = "griddap",
+    url = "https://www.ncei.noaa.gov/erddap/"
+) %>% View()
+
+ed_search_adv(
+    query = "chl",
     minLat = -25,
     maxLat = -15,
     minLon = 115,
@@ -50,7 +84,7 @@ ed_search_adv(
 
 
 ed_search_adv(
-    query = 'salinity',
+    query = "sss",
     minLat = -25,
     maxLat = -15,
     minLon = 115,
@@ -59,35 +93,59 @@ ed_search_adv(
     url = "https://coastwatch.pfeg.noaa.gov/erddap/"
 ) %>% View()
 
-ed_search_adv(
-    query = 'salinity',
-    minLat = -25,
-    maxLat = -15,
-    minLon = 115,
-    maxLon = 125,
-    protocol = "griddap",
-    url = "https://www.ncei.noaa.gov/erddap/"
-) %>% View()
 
 test_chl_a <- getNCDF(
     ds_ids = c(
         # Aqua MODIS
         "erdMH1chla1day",
         # SeaWiFS
-        "erdSW2018chla1day"#,
+        "erdSW2018chla1day" # ,
         # Nasa
-        #"erdVH2018chla1day"
-    ), 
-    time_range = c("2002-01-01", "2004-01-01"), 
-    lat_range = c(-25, -15), 
+        # "erdVH2018chla1day"
+    ),
+    time_range = c("2002-01-01", "2004-01-01"),
+    lat_range = c(-25, -15),
     lon_range = c(115, 125)
-    
 )
 
-ras_clean <- ncdfToRast(ncdf_list = test_chl_a, var = "chl")
+chl_a_ras <- ncdfToRast(ncdf_list = chl_a_nc, var = "chl")
 
 rastStackToMov(
     ras_clean,
     shape_file = terra::vect("./data/Australia_shp/STE11aAust.shp"),
     output_file = "./static/test.gif"
 )
+
+
+writeRaster(
+    chl_a_ras,
+    filename = "./outputs/chl_a_2018-2022.tif"
+)
+
+nc <- nc_open("https://projects.pawsey.org.au/mma-species-distribution/test.nc?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=P0UZLYNCYTVQECBH2L9I%2F20230929%2Fdefault%2Fs3%2Faws4_request&X-Amz-Date=20230929T081654Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=b066bec3a26fcbc25aa686e3f09cbeb48c168b5d329dcdaa88514d89c791372a")
+
+
+ ## setup the connection
+
+library(aws.s3)
+b <- get_bucket("mma-species-distribution", region = "")
+b
+
+nc <- s3read_using(
+    FUN = ncdf4::nc_open(),
+    object = "s3://mma-species-distribution/test.nc",
+    region = ""
+)
+
+con <- s3connection(
+    "test.nc",
+    bucket = b,
+    region = ""
+)
+
+s3read_using()
+nc <- ncdf4::nc_open(con)
+
+
+close(con)
+
